7a688c203eed91525eee16d80003550e55919ad8
Bapuji Pradhan <bapujipradhan07@gmail.com>
2024-01-29 16:42:17 +0530

    add

---
+++ Day 1/EC.js
@@ -0,0 +1,6 @@
+//When you run a javascript programm at first a execution context will be created.
+// Everything in js happens inside a execution context
+//Execution context contains two components inside it 
+//first is memory creation component or variable cration environment here js will alocate memory to variable and function and they are stored in key and value pair .
+//second is code component or thread of execution here code executed one line at a time
+//js is a synchronous single threaded language
